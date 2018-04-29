eaafb780b3f75a3c8720fbe17c220c090ab1d442
xiMBest <vitalik.nayda@gmail.com>
2018-04-29 16:22:28 +0300

    added reminder design

---
+++ app/src/main/java/com/example/android/meter/Alarm.java
@@ -0,0 +1,13 @@
+package com.example.android.meter;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Alarm extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_reminder);
+    }
+}
